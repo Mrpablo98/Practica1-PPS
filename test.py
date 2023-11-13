7a719a25732a9b81917ea409b427be9a6f3f17cf
Mrpablo98 <pablosandev@gmail.com>
2023-11-13 21:16:04 +0100

    comentarios explicativos no test

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import unittest
 from fibo import Fibonacci
-
+#Instanciamos a clase TestFibonacci que hereda da clase TestCase
 class TestFibonacci(unittest.TestCase):
+    #Definimos a función do test que se asegure de que o 5 elemento da sucesión é igual a 3
     def test_quinto_elemento_fibonacci(self):
         result=Fibonacci(5)
         self.assertEqual(result[4], 3)
